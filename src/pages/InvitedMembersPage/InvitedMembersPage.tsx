0d6c52df8cf400babdfb2b085da36322da162655
myktsk <kerokerokerori.1215@gmail.com>
2020-12-08 20:14:09 +0900

    :sparkles: apply composition in pages

--- src/pages/InvitedMembersPage/InvitedMembersPage.tsx
+++ src/pages/InvitedMembersPage/InvitedMembersPage.tsx
@@ -3,16 +3,29 @@ import { INVITATIONS } from '../../mocks/invitations'
 import { MemberList } from '../../components/MemberList'
 import { MemberListItem } from '../../components/MemberList/MemberListItem'
 
+const PLACEHOLDER = "https://avatars.dicebear.com/api/initials/test.svg";
 export class InvitedMembersPage extends React.Component {
   deleteInvitation = () => { console.log('招待を削除') }
+
+  renderMenuChildren = (): React.ReactNode => (
+    <button onClick={this.deleteInvitation}>削除する</button>
+  );
+
   render = () => (
     <>
       <h1>招待メンバー一覧ページ</h1>
       <MemberList>
         {
           INVITATIONS.map(member => (
-            <MemberListItem key={member.id} member={member}>
-              <button onClick={this.deleteInvitation}>削除する</button>
+            <MemberListItem
+              key={member.id}
+              menuChildren={this.renderMenuChildren()}>
+              <img
+                  src={PLACEHOLDER}
+                  style={{width: 40}}
+                  alt="avatar" 
+                />
+              <div>{member.email}</div>
             </MemberListItem>
           ))
         }
--- src/pages/JoinedMembersPage/JoinedMembersPage.tsx
+++ src/pages/JoinedMembersPage/JoinedMembersPage.tsx
@@ -1,26 +1,44 @@
 import React from 'react'
+import { Member } from '../../types';
 import { JOINED_MEMBERS } from '../../mocks/members'
 import { MemberList } from '../../components/MemberList'
 import { MemberListItem } from '../../components/MemberList/MemberListItem';
 
 const currentUser = { id: 'member1' };
+const PLACEHOLDER = "https://avatars.dicebear.com/api/initials/test.svg";
 
 export class JoinedMembersPage extends React.Component {
   leave = () => { console.log('退出') }
 	makeOtherLeave = () => { console.log('メンバーを削除') }
 
+  renderMenuChildren = (member: Member): React.ReactNode | null => {
+    if(member.role === "admin") return null;
+    return (
+      <button onClick={ member.id === currentUser.id ? this.leave : this.makeOtherLeave} >
+        {member.id === currentUser.id ? "退出する" : "退出させる"}
+      </button>
+    )
+  }
+  
   render = () => (
     <>
       <h1>参加メンバー一覧ページ</h1>
       <MemberList>
         {
           JOINED_MEMBERS.map(member => (
-            <MemberListItem key={member.id} member={member}>
-              {member.role !== "admin" && (
-                <button onClick={ member.id === currentUser.id ? this.leave : this.makeOtherLeave} >
-                  {member.id === currentUser.id ? "退出する" : "退出させる"}
-                </button>
-              )}
+            <MemberListItem
+              key={member.id}
+              menuChildren={this.renderMenuChildren(member)}>
+                <img
+                  src={member.avatar ? member.avatar : PLACEHOLDER}
+                  style={{width: 40}}
+                  alt="avatar" 
+                />
+                <div>
+                  <div>{member.name}</div>
+                  <div>{member.email}</div>	
+                </div>
+                {member.role === "guest" && <div>ゲスト</div>}
             </MemberListItem>
           ))
         }
