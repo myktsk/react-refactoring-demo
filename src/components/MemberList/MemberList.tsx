0ea78adf9bca8746d35b1b03d43c2941f47ae26e
myktsk <kerokerokerori.1215@gmail.com>
2020-12-08 20:27:12 +0900

    :construction: accept only member item in member list

--- src/components/MemberList/MemberList.tsx
+++ src/components/MemberList/MemberList.tsx
@@ -1,3 +1,15 @@
 import React from 'react'
+import { MemberListItem } from './MemberListItem';
 
-export const MemberList: React.FC = ({ children }) => ( <ul>{ children }</ul> );
+export class MemberList extends React.Component {
+	static ListItem = MemberListItem;
+
+	render = () => {
+    const { children } = this.props;
+
+    return React.Children.map(children, child => {
+      if (!React.isValidElement(child) || child.type !== MemberList.ListItem) return null; 
+			return React.cloneElement(child);
+	  })
+	}
+}
