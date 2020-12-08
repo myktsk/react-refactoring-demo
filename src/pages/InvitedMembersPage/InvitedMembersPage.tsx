1e9f5aa6929937198ebe9f6ee4346748c3a79986
myktsk <kerokerokerori.1215@gmail.com>
2020-12-08 19:53:19 +0900

    :sparkles: apply new composition structure in pages

--- src/pages/InvitedMembersPage/InvitedMembersPage.tsx
+++ src/pages/InvitedMembersPage/InvitedMembersPage.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { INVITATIONS } from '../../mocks/invitations'
 import { MemberList } from '../../components/MemberList'
+import { MemberListItem } from '../../components/MemberList/MemberListItem'
 
-export const InvitedMembersPage:React.FC = () => {
-  return (
+export class InvitedMembersPage extends React.Component {
+  deleteInvitation = () => { console.log('招待を削除') }
+  render = () => (
     <>
       <h1>招待メンバー一覧ページ</h1>
-      <MemberList members={INVITATIONS} />
+      <MemberList>
+        {
+          INVITATIONS.map(member => (
+            <MemberListItem key={member.id} member={member}>
+              <button onClick={this.deleteInvitation}>削除する</button>
+            </MemberListItem>
+          ))
+        }
+      </MemberList>
     </>
   )
 }
--- src/pages/JoinedMembersPage/JoinedMembersPage.tsx
+++ src/pages/JoinedMembersPage/JoinedMembersPage.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { JOINED_MEMBERS } from '../../mocks/members'
 import { MemberList } from '../../components/MemberList'
+import { MemberListItem } from '../../components/MemberList/MemberListItem';
 
-export const JoinedMembersPage = () => {
-  return (
+const currentUser = { id: 'member1' };
+
+export class JoinedMembersPage extends React.Component {
+  leave = () => { console.log('退出') }
+	makeOtherLeave = () => { console.log('メンバーを削除') }
+
+  render = () => (
     <>
       <h1>参加メンバー一覧ページ</h1>
-      <MemberList members={JOINED_MEMBERS} />
+      <MemberList>
+        {
+          JOINED_MEMBERS.map(member => (
+            <MemberListItem key={member.id} member={member}>
+              {member.role !== "admin" && (
+                <button onClick={ member.id === currentUser.id ? this.leave : this.makeOtherLeave} >
+                  {member.id === currentUser.id ? "退出する" : "退出させる"}
+                </button>
+              )}
+            </MemberListItem>
+          ))
+        }
+      </MemberList>
     </>
   )
 }
